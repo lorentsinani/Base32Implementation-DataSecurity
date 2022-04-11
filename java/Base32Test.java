18a86d52ebd6394eb3759234b265b18c9b57307f
Lorik Mustafa <77617510+lorikmustafa@users.noreply.github.com>
2022-04-11 14:31:07 +0200

    Update Base32Test.java

--- java/Base32Test.java
+++ java/Base32Test.java
@@ -13,12 +13,46 @@ class Base32Test {
       System.out.println("Jepni nje input i cili do te enkodohet: ");
       String theInput;
       theInput = input.nextLine();
+	   
+	   
+//nese deshirojme me e be me CMD
+//      if (a.length<1) {
+//         System.out.println("Usage:");
+//         System.out.println("java base32Test 1/2/3/4/5");
+//         return;
+//      }
+	   
+//      String theExpected = "IE======";
+//      String test = a[0];
+//      String theInput = null;
+//      String theExpected = null;
+//      if (test.equals("1")) {
+//         theInput = "A";
+//         theExpected = "IE======";
+//      } else if (test.equals("2")) {
+//         theInput = "AB";
+//         theExpected = "IFBA====";
+//      } else if (test.equals("3")) {
+//         theInput = "ABC";
+//         theExpected = "IFBEG===";
+//      } else if (test.equals("4")) {
+//         theInput = "ABCD";
+//         theExpected = "IFBEGRA=";
+//      } else if (test.equals("5")) {
+//         theInput = "ABCDE";
+//         theExpected = "IFBEGRCF";
+//      } else {
+//         System.out.println("Usage:");
+//         System.out.println("java BitpediaBase32Test 1/2/3/4/5");
+//         return;
+//      }
+//      System.out.println("Expected: "+theExpected);
 
       String theEncoded = base32Imp.encode(theInput.getBytes());
       byte[] theDecoded = base32Imp.decode(theEncoded);
       System.out.println("Input   : "+theInput);
       System.out.println("Encoded : "+theEncoded);
-//      System.out.println("Expected: "+theExpected);
+
       System.out.println("Decoded : "+new String(theDecoded));
       input.close();
       return;
